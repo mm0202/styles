aff4e21a558d86e843f9a024c709f14318c51599
mm0202 <for_etc@hotmail.com>
2022-05-07 15:27:27 +0900

    update packages

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,5 @@
 const gulp = require('gulp');
-const sass = require('gulp-sass');
+const sass = require('gulp-sass')(require('sass'));
 const postcss = require('gulp-postcss');
 const sourcemaps = require('gulp-sourcemaps');
 const autoprefixer = require('autoprefixer');
@@ -9,17 +9,19 @@ const paths = {
     'css': 'docs/'
 };
 
-gulp.task('scss', function () {
+gulp.task('scss', function (done) {
     return gulp.src(paths.scss + '**/*.scss')
         .pipe(sourcemaps.init())
         .pipe(sass())
         .pipe(sourcemaps.write())
         .pipe(postcss([autoprefixer()]))
         .pipe(gulp.dest(paths.css))
+        done();
 });
 
-gulp.task('watch', function () {
+gulp.task('watch', function (done) {
     gulp.watch(paths.scss + '**/*.scss', gulp.series('scss'));
+    done();
 });
 
 gulp.task('default', gulp.parallel('watch'));
